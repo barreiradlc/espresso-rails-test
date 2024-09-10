94c1a3cc9fb9ad9675e64423655ae0a67bc971f7
Augusto Barreira <barreira266@hotmail.com>
2024-09-10 10:01:48 -0300

    Backend - CRUD funcionários, cadastro de funcionário
    Relação de usuários e empresa ( factory )

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -7,5 +7,6 @@ FactoryBot.define do
     role { %w[admin employee].sample }
     password { 'password' }
     password_confirmation { 'password' }
+    company { FactoryBot.create(:company) }
   end
 end
