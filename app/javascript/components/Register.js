a634c38d889a7c98e280b954fe846c00f377432b
Augusto Barreira <barreira266@hotmail.com>
2024-09-13 01:02:54 -0300

    Frontend - Register
    Rota básica incusa

---
+++ app/javascript/components/Register.js
@@ -0,0 +1,9 @@
+// import { RegisterPageController } from './pages/register/controller'
+
+const Register = () => {
+  return (    
+     <h3>RegisterPageController</h3>
+  )
+}
+
+export default Register
--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # FRONTEND
   root to: 'home#index'
+  get '/register_page', to: 'register_page#index'
   get '/login_page', to: 'login_page#index'
 
   # AUTH
   post :login, to: 'create_session#call'
 
   # USERS
-  post :employee, to: 'create_employee#call'
   put 'employee/:id', to: 'update_employee#call'
+  post :employee, to: 'create_employee#call'
   delete 'employee/:id', to: 'delete_employee#call'
 
   # CARDS
