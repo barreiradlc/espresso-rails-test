79984f8f90460df2ca32728c9ce5376ee5d09db6
Augusto Barreira <barreira266@hotmail.com>
2024-09-13 01:53:27 -0300

    Frontend - Register
    Controller

---
+++ app/javascript/components/pages/register/controller.js
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+
+
+function RegisterPageController() {  
+  const [registerPayload, setRegisterPayload] = useState({ email: '', password: '', company: { name: '', cnpj: '' }});
+  const [errors, setErrors] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  function handleChangeInput(event) {
+    const { id: attribute, value } = event.target
+    
+    setRegisterPayload({
+      ...registerPayload,
+      [attribute]: value
+    })
+  }
+
+
+  async function handleRegister() {
+    setLoading(true)
+    try {
+      // const errorsCaptured = validateRegisterForm(loginPayload)
+      const errorsCaptured = {}
+      
+      
+      if (Object.keys(errorsCaptured).length) {
+        setErrors(errorsCaptured)
+        return;
+      }
+
+      // const data = await createUser(loginPayload)
+      const data = {}
+
+      if (data.token) {
+        localStorage.setItem("@expressoApp:jwt", JSON.stringify(data.token));
+        document.cookie = `@expresso_app:jwt=${JSON.stringify(data.token)}`
+      }
+    } catch (error) {
+      alert(error)
+      throw new Error("Erro ao realizar cadastro de usuário, tente novamente mais tarde!");
+    }
+    setLoading(false)
+  }
+
+  console.log({
+    handleChangeInput,
+    registerPayload,
+    handleRegister,
+    loading,
+    errors,
+  })
+
+  return <h4>controller</h4>
+
+  // return <RegisterPage
+  //   handleChangeInput={handleChangeInput}
+  //   registerPayload={registerPayload}
+  //   handleRegister={handleRegister}
+  //   loading={loading}
+  //   errors={errors}
+  // />
+}
+
+export { RegisterPageController };
+
